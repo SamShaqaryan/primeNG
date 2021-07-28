80da7dc28744a78df2c7962d17a3a18873187826
SamShaqaryan <shaqaryan.sam@gmail.com>
2021-07-28 15:48:44 +0400

    Full table users and posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from './user.service';
+import { Posts, UserService } from './user.service';
 import { Users } from './user.service';
 
 @Component({
@@ -10,21 +10,27 @@ import { Users } from './user.service';
 export class AppComponent implements OnInit {
 
 
-user:Users[] =[]
+  posts: Posts[] = []
+  user: Users[] = []
 
 
-  constructor(private userService:UserService) { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
 
+    this.userService.getInfo().subscribe(res => {
+      this.user = res
+    })
 
-   this.userService.getinfo().subscribe(res=> {
-    this.user = res
-    console.log(this.user)
-   })
 
-   
   }
- 
+
+  getPosts(event: any) {
+    this.posts = [];
+    this.userService.getPosts(event.data.id).subscribe(res => {
+      this.posts = res;
+    })
+  }
+
 
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -2,11 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 export interface Users {
-  id:number,
-  name: string,
+  id: string,
+  name:string,
 username:string,
-email:string
+email:string,
+address: {
+  street:string,
+  suite:string,
+  city:string,
+  
+}}
+export interface Posts {
+  userId:number,
+  id:number,
+  title:string,
+  body:string
 }
+
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -16,14 +30,14 @@ export class UserService {
 
   constructor(private http:HttpClient) { }
 
-   getinfo(){
+   getInfo(){
      return this.http.get<Users[]>('https://jsonplaceholder.typicode.com/users')
    
-
-
   }
 
-
+getPosts (id:any){
+  return this.http.get<Posts[]>('https://jsonplaceholder.typicode.com/user/'+id+'/posts')
+}
 
 
 
